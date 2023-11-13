c405b84d204b7154ce968bf9538b36fa9a34df33
David B <david.b62@gso.schule.koeln>
2023-11-13 12:55:17 +0100

    Tests01

---
+++ Verlag/Buch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Verlag;
+
+namespace Verlag
+{
+    public class Buch
+    {
+        private string autor;
+        private string titel;
+        private int auflage;
+
+        public Buch(string autor, string titel, int auflage)
+        {
+            this.autor = autor;
+            this.titel = titel;
+            if (auflage > 0)
+                this.auflage = auflage;
+            else throw new ArgumentOutOfRangeException("auflage zu klein", nameof (auflage));
+        }
+        public Buch(string autor, string titel)
+        {
+            this.autor = autor;
+            this.titel = titel;
+            auflage = 1;
+        }
+
+        public string Autor { get { return autor; } set { autor = value; } }
+        public string Titel { get { return titel; } }
+        public int Auflage { get { return auflage; } 
+            set { 
+                if (auflage > 0)
+                auflage = value; 
+                else throw new ArgumentOutOfRangeException("auflage zu klein", nameof (auflage));
+            }
+        }
+
+
+    }
+}
