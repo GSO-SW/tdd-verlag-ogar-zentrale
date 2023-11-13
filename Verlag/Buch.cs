dddc5cb674be332d9d570e984765fab619a24023
BlyZeYT <leonschimmel15@gmail.com>
2023-11-13 12:11:19 +0100

    Buch implementiert

---
+++ Verlag/Buch.cs
@@ -0,0 +1,41 @@
+﻿namespace Verlag;
+
+public sealed class Buch
+{
+    private string autor = null!;
+    private int auflage;
+
+    public string Autor
+    {
+        get => autor;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value)
+                || value.Any(x => x is '#' or ';' or '§' or '%'))
+                throw new ArgumentException("Der Autor darf keine verbotenen Zeichen beinhalten", nameof(value));
+
+            autor = value;
+        }
+    }
+
+    public string Titel { get; }
+
+    public int Auflage
+    {
+        get => auflage;
+        set
+        {
+            if (value < 1)
+                throw new ArgumentOutOfRangeException(nameof(value), "Die Auflage muss mindestens 1 sein");
+
+            auflage = value;
+        }
+    }
+
+    public Buch(string autor, string titel, int auflage = 1)
+    {
+        Autor = autor;
+        Titel = titel;
+        Auflage = auflage;
+    }
+}
