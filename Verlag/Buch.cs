87683bbddb7eac30a8a33b072c4332eca3e41810
David B <david.b62@gso.schule.koeln>
2023-11-23 10:32:46 +0100

    asdf

--- Verlag/Buch.cs
+++ Verlag/Buch.cs
@@ -12,8 +12,8 @@ namespace Verlag
 
         public Buch(string autor, string titel)
         {
-            if (autor is null or "" || autor.Any(c => "#;§%".Contains(c)))  
-                throw new ArgumentException("Autorname ungültig", nameof(autor));
+            if (autor is null or "" || autor.Any(c => "#;§%".Contains(c)))
+            { throw new ArgumentException("Autorname ungültig", nameof(autor)); }
             else this.autor = autor;
 
             this.titel = titel;
@@ -23,8 +23,8 @@ namespace Verlag
         public Buch(string autor, string titel, int auflage) :this (autor, titel)
         {
             if (auflage > 0)
-                this.auflage = auflage;
-            else throw new ArgumentOutOfRangeException("auflage zu klein", nameof(auflage));
+            { this.auflage = auflage; }
+            else { throw new ArgumentOutOfRangeException("auflage zu klein", nameof(auflage)); }
         }
         public Buch(string autor, string titel, int auflage, string ISBN) :this(autor, titel, auflage)
         {
@@ -37,24 +37,24 @@ namespace Verlag
         public string Autor { get { return autor; } set { autor = value; } }
         public string Titel { get { return titel; } }
         public int Auflage { get { return auflage; } 
-            set { 
+            set {
                 if (value > 0)
-                auflage = value; 
-                else throw new ArgumentOutOfRangeException("auflage zu klein", nameof (auflage));
+                { auflage = value; }
+                else { throw new ArgumentOutOfRangeException("auflage zu klein", nameof(auflage)); }
             }
         }
-        public string ISBN { get { return iSBN; } 
+        public string ISBN { get { return iSBN; }
             set {
                 if (value.Length == 17 && value.Count(c => c == '-') == 4)
-                    this.iSBN = new string(value.Where(c => c != '-').ToArray());
+                { this.iSBN = new string(value.Where(c => c != '-').ToArray()); }
 
                 else if (value.Length == 13)
-                    this.iSBN = value;
+                { this.iSBN = value; }
 
                 else if (value.Length == 12)
                 {
                     if (value.Contains("-"))
-                        value = new string(value.Where(c => c != '-').ToArray());
+                    { value = new string(value.Where(c => c != '-').ToArray()); }
                     int pruefsumme = 0;
                     for (int i = 0; i < value.Length; i += 2)
                     {
@@ -68,4 +68,3 @@ namespace Verlag
         }
 
     }
-}
